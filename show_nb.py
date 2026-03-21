1c9ace9a4547bc31b0d0542921825dc877792014
trpa-mason <mbindl@trpa.org>
2026-03-21 00:25:49 -0700

    updates

--- show_nb.py
+++
@@ -1,8 +0,0 @@
-import json, sys
-with open(r'c:\Users\mbindl\Documents\GitHub\Reporting\update_existing_residential.ipynb', encoding='utf-8') as f:
-    nb = json.load(f)
-for i, c in enumerate(nb['cells']):
-    src = ''.join(c['source'])
-    first = src[:90].replace('\n', ' ')
-    line = f"[{i:02d}] {c['cell_type']:8} id={c['id']:12}  {first}\n"
-    sys.stdout.buffer.write(line.encode('utf-8'))
